24c7d417b570dba952ada0bda4c47af7a39536b3
himeno-yusuke <himeno-yusuke@kayac.com>
2019-02-16 15:34:25 +0900

    更新

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,10 +1,9 @@
 aws s3 sync \
-    --dryrun \
     --profile netlify-techbook \
     --exact-timestamps \
     --delete \
     --acl public-read \
     --cache-control "no-cache" \
     --exclude "**/.*" \
-    ./docs \
+    ./dist \
     s3://netlify-book/
