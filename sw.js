2f54a9cf07fcc801f9e04605991575814899e1ba
himeno-yusuke <himeno-yusuke@kayac.com>
2019-02-16 16:08:31 +0900

    Updates

---
+++ sw.js
@@ -0,0 +1,171 @@
+importScripts('/nuxtjs-on-github-pages/_nuxt/workbox.4c4f5ca6.js', 'custom-sw.js')
+
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/064120721cb016345a4d.js",
+    "revision": "ec1c43c1e1769b453e03b4023b396d30"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/093839fa332c5a7c8037.js",
+    "revision": "d9b8ac08772891710a7f877afbf25a03"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/0b4ad5922dd94e562f1e.css",
+    "revision": "c6b3b2f89c2662babe61dadbdce326cf"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/0e4338761429b4eb16ac.css",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/1b3dda4cfd5112892b1f.js",
+    "revision": "0e981b2aeab92a11e504f61a3ce096cb"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/1d77e36bfaf4096e9780.js",
+    "revision": "13fe5711b64ee3c7a633e3c0eb509857"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/24bbf0d9c769ead5eb9e.js",
+    "revision": "58feafd0983386a8516fdae018c8b836"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/3b2de5c543274086916a.css",
+    "revision": "52547398194e09b936fccf89f2c66e37"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/3f4eac2dce17f6d01105.js",
+    "revision": "4b30cb31a5313f1e96b57233c4c8146b"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/4e157051666d0935c86b.css",
+    "revision": "629f8357f83041537ff082496c844301"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/4f4fb2519cc40ad9f2e0.css",
+    "revision": "ad964101dc382245ecbb33ac867a22b5"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/59b2d8ea80011bfd3679.js",
+    "revision": "b8b4fa844bc587b4ce209cf8796b588f"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/64918d9fe32ca0d56a18.js",
+    "revision": "a6136052110f426bd8739ba551621d3c"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/64f66d2878d032261053.css",
+    "revision": "667c5c61df5728857cada8f90e64ce5c"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/696ea1daa80ced183b86.js",
+    "revision": "e820fe3bb1fbcc2e2ea18a380c104eff"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/6eb7bfc61b02947e81fb.css",
+    "revision": "f758b88ce6a545023ea7eadccff0abf3"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/7e78a9261a3723793d62.js",
+    "revision": "d624170f21c73cfc7c2c6f5124b22729"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/88dd51f1c8d30fc587fe.css",
+    "revision": "5fe4c3108fbb8287601d748065c425db"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/8d3ed16efbd542ad2699.js",
+    "revision": "918b5bfff67552630424a08402a68aa8"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/9598c174a80a278ebe77.js",
+    "revision": "105871e9d659584f92550a5db526a465"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/a75d1fff356feeecdafe.css",
+    "revision": "c27d2917c3efa0d0bbb42702891bd686"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/b19d1991575b5beba980.js",
+    "revision": "a4664814e58d42f01818faa562f8943f"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/b393d0689b9c4ca582b8.js",
+    "revision": "7603142b98fcf4edaeb630f99466a4ab"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/b54d36308fbeb448b50e.css",
+    "revision": "8cfe61add7e0f8de7bce99941ff01387"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/b807a5cf7bbef0ccc534.css",
+    "revision": "7263a67913568b463dce7e0f1d89f3b1"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/be114672403339f61f60.css",
+    "revision": "5f56b55d83ea31eb4f639cfb35000b6b"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/c38fbb699c8ff3f12853.js",
+    "revision": "8cf2ec8620469b708c8d7c173f89612f"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/ca2abb884837f33d058f.css",
+    "revision": "a825d51c430407d9a7753d9af2da5c3a"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/cfc4fa4f73bd9b28d00c.css",
+    "revision": "aa108afee5a1f042832a9c2885b3a2c9"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/d83fbff8b371de5e12da.css",
+    "revision": "7e4040a37d59e2f766c35494681c7cac"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/e2933058242d1d7a0ae8.js",
+    "revision": "e0976972334eb6fc6ec61c4cf73cb4ea"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/e57654431899a8469e5c.js",
+    "revision": "f0cf3c9941d322bf435d65792e613b19"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/e7ea9cf498b189a8eaf0.css",
+    "revision": "e4a62f90773097ad7f99833e2d8cfad8"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/ee6137c2afa57bd7797b.js",
+    "revision": "208d1dac4e0ed30753a049955010fd56"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/eee4d624de2e54e7debe.js",
+    "revision": "1a1dc1e1b4cdd61942de81f689c50e3a"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/f4f02ede605167a5d334.js",
+    "revision": "ee1943d968f82034ab7af32c53a12966"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/f5bf90f142f4ad0d1cf2.js",
+    "revision": "bde844719e5e708bf610e28c033df532"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/f7125c16520a272d86f8.css",
+    "revision": "570afcecb6e66862c37af3f27c01e0b9"
+  },
+  {
+    "url": "/nuxtjs-on-github-pages/_nuxt/f8599319a527aafa5ebe.js",
+    "revision": "7d214a1658da55f7f3842b8ebfa5c4d1"
+  }
+], {
+  "cacheId": "hyme",
+  "directoryIndex": "/",
+  "cleanUrls": false
+})
+
+workbox.clientsClaim()
+workbox.skipWaiting()
+
+workbox.routing.registerRoute(new RegExp('/nuxtjs-on-github-pages/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+
+workbox.routing.registerRoute(new RegExp('/nuxtjs-on-github-pages/.*'), workbox.strategies.networkFirst({}), 'GET')
